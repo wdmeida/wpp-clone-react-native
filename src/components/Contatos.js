5d487045cfc05252a5b7077928a4d4452d144253
Wagner Almeida <wdmeida@gmail.com>
2017-09-20 09:13:41 -0300

    Creating ListaContatosReducers and all structure to listing the contacts.

--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default props => (
-  <View style={styles.container}>
-    <Text>Contatos</Text>
-  </View>
-);
+import { contatosUsuarioFetch } from '../actions/AppActions';
+
+class Contatos extends Component {
+
+  componentWillMount() {
+    this.props.contatosUsuarioFetch();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Contatos</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   }
 });
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  contatosUsuarioFetch
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(Contatos);
